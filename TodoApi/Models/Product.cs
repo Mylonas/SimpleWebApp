3d05877747f91b3eebfa66231414d396e3dfe980
mikmy <mikmy@LAPTOP-D2VPQIIS>
2024-11-06 21:18:42 +0000

    - delete demo code - added atributes

--- TodoApi/Models/Product.cs
+++ TodoApi/Models/Product.cs
@@ -4,5 +4,6 @@ public class Product
 {
     public long Id { get; set; }
     public string? Name { get; set; }
-    public bool IsComplete { get; set; }
+    public decimal price { get; set; }
+    public string? category { get; set; }
 }
